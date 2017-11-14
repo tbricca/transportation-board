46c16c890d8b081763ce05aea751d3440950c243
tbricca <tbricca@gmail.com>
2017-11-14 15:01:33 -0800

    working on adding bus api

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,15 +1,44 @@
 import React, { Component } from 'react';
 import axios from "axios";
+import $ from "jquery";
 
+
+var cityMapper = "https://developer.citymapper.com/api/1/singlepointcoverage/?coord=51.578973%2C-0.124147&key=8d3b8bb18fc668fdf342d8e09fcf3249";
 
 class Home extends Component {
     constructor(props) {
         super(props)
-
+        this.state = {
+            busRoutes: []
+        }
     }
+    handleCityMapperSubmit = (e) => {
+        $.ajax({
+            url: cityMapper,
+            type: "GET",
+            data: {},
+            dataType: 'json',
+            success: function(data) {
+                console.log(data);
+            },
+            error:function(err) {alert(err); }
+        });
+    }
+    
+
+    // componentDidMount() {
+    //     fetch('/bus-routes')
+    //     .then(busRoutes => {
+    //         console.log( 'busRoutes');
+    //         this.setState({ busRoutes });
+    //     });
+    // }
     render() {
         return (
             <div>
+            <h1>Transit Board</h1>
+            <h4> A board to track all the transit around you </h4>
+            <input className="searchbox" type='text' placeholder="Input your address here..." />
             <p> This is where the api's will start to go </p>
           </div>
         )
