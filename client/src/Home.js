32eb2dad55e3f60aa78945a9b8c787be5015f8db
tbricca <tbricca@gmail.com>
2017-11-15 20:41:52 -0800

    got address autocomplete to work, need to pass it through to bus routes

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import $ from "jquery";
+import Search from "./Search";
 
 var axios = require('axios');
 var cityMapper = "https://developer.citymapper.com/api/1/singlepointcoverage/?coord=51.578973%2C-0.124147&key=8d3b8bb18fc668fdf342d8e09fcf3249";
@@ -44,9 +45,7 @@ class Home extends Component {
         // 
         return (
             <div>
-            <h1>Transit Board</h1>
-            <h4> A board to track all the transit around you </h4>
-            <input className="searchbox" type='text' placeholder="Input your address here..." />
+            <Search />
             <p> Agency: {agency} Bus Routes: {routes} Route Description: {routeDescription} </p>
             <p>Place second api here</p>
             </div>
--- client/src/Search.js
+++ client/src/Search.js
@@ -1,46 +1,76 @@
 import React, { Component } from 'react';
 import $ from "jquery";
+import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete'
+/* global google */
 
+var axios = require('axios');
 
-class Home extends Component {
+class Search extends Component {
     constructor(props) {
         super(props)
-        this.state = {
-            location: ""
-        }
-    }
-    handleSubmit = (e) => {
-        var getLocation = axios.get('https://maps.googleapis.com/maps/api/geocode/json?address='+this.state.location+'&key='+'AIzaSyACjP02sW8yzMCfY0Nm7289ZZrDKFpxOxo')
-        .then((latLng) => {
-          console.log("latLng: ", latLng);
-          tempLocationLat = latLng.data.results[0].geometry.location.lat;
-          tempLocationLng = latLng.data.results[0].geometry.location.lng;
-          counter++;
-        });
-    }
-    handleLocationChange = (e) => {
-        this.setState({location: e.target.value});
-    }
+        this.state = { address: 'San Francisco, CA' }
+        this.onChange = (address) => this.setState({ address })
+      }
+    
+      handleFormSubmit = (event) => {
+        event.preventDefault()
+    
+        geocodeByAddress(this.state.address)
+          .then(results => getLatLng(results[0]))
+          .then(latLng => console.log('Success', latLng))
+          // get call to upload latLong
+        //   .then(latln axios.get("/bus-Routes"))
+          .catch(error => console.error('Error', error))
+      }
+    // constructor(props) {
+    //     super(props)
+    //     this.state = {
+    //         location: ""
+    //     }
+    // }
+    // handleSubmit = (e) => {
+    //     let tempLocationLat = "";
+    //     let tempLocationLng = "";
+    //     var getLocation = axios.get('https://maps.googleapis.com/maps/api/geocode/json?address='+this.state.location+'&key='+'AIzaSyACjP02sW8yzMCfY0Nm7289ZZrDKFpxOxo&libraries=places&callback=initAutocomplete')
+    //     .then((latLng) => {
+    //       console.log("latLng: ", latLng);
+    //       tempLocationLat = latLng.data.results[0].geometry.location.lat;
+    //       tempLocationLng = latLng.data.results[0].geometry.location.lng;
+          
+    //     });
+    // }
+    // handleLocationChange = (e) => {
+    //     this.setState({location: e.target.value});
+    // }
 
     render () {
+        const inputProps = {
+            value: this.state.address,
+            onChange: this.onChange,
+        }
         return(
             <div>
                 <h1>Transit Board</h1>
                 <h4> A board to track all the transit around you </h4>
+                <form onSubmit={this.handleFormSubmit}>
+                    <PlacesAutocomplete inputProps={inputProps} />
+                    <button type="submit">Submit</button>
+                </form>
                 <input className="searchbox" type='text' placeholder="Search by city..." value={this.state.location} onChange={this.handleLocationChange} />
             </div>
         )
     }
         
-
-
-
-
-
 }
-        var input = document.getElementById('pac-input');
-        var searchBox = new google.maps.places.SearchBox(input);
-        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+        
+        
+// geocodeByAddress(this.state.address)
+// .then(results => getLatLng(results[0]))
+// .then(latLng => console.log('Success', latLng))
+
+// .catch(error => console.error('Error', error))
+
+// }
 
         
 
